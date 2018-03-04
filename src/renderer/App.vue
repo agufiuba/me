4096f5480f57d2ea8b55c3ba86cbcc9d33b0b02e
agu <agu@agus-MacBook-Pro.local>
2018-03-04 00:51:08 -0300

    WIP

---
+++ src/renderer/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>ASDASD</h1>
+    <nav class="navbar navbar-dark bg-warning" style="background-color: #e3f2fd;">
+      <a class="navbar-brand" href="#">Navbar</a>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'my-project',
+  };
+</script>
+
+<style lang="scss">
+  @import '../custom'
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar navbar-dark bg-warning" style="background-color: #e3f2fd;">
+      <a class="navbar-brand" href="#">Navbar</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation';
+
+  export default {
+    name: 'landing-page',
+    components: { SystemInformation },
+    methods: {
+      open(link) {
+        this.$electron.shell.openExternal(link);
+      },
+    },
+  };
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+</style>
