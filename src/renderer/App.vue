7695478d3ff85a161347ed8500e61a119bedd45b
agu <agu@agus-MacBook-Pro.local>
2018-03-06 09:25:31 -0300

    Preview

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <h1>ASDASD</h1>
-    <nav class="navbar navbar-dark bg-warning" style="background-color: #e3f2fd;">
-      <a class="navbar-brand" href="#">Navbar</a>
-    </nav>
     <router-view></router-view>
   </div>
 </template>
--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,8 +1,51 @@
 <template>
   <div id="wrapper">
-    <nav class="navbar navbar-dark bg-warning" style="background-color: #e3f2fd;">
-      <a class="navbar-brand" href="#">Navbar</a>
+    <nav class="navbar navbar-light bg-warning" style="margin-bottom: 10px">
+      <div class="container">
+        <a class="navbar-brand" href="#">Me!</a>
+      </div>
     </nav>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-8">
+          <h1>Passwords</h1>
+        </div>
+        <div class="col-12 col-sm-4">
+          <input class="form-control" type="text" placeholder="Buscar..." style="margin-top: 10px"/>
+        </div>
+      </div>
+      <div class="row"><div class="col-12"><hr/></div></div>
+      <div class="row">
+        <div class="col-12">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Domain</th>
+                <th scope="col">User</th>
+                <th scope="col">Mail</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Google</th>
+                <td>agugaillard@gmail.com</td>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="row">Amazon</th>
+                <td>agugaillard@gmail.com</td>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="row">Steam</th>
+                <td>agugaillard</td>
+                <td>agugaillard@gmail.com</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
